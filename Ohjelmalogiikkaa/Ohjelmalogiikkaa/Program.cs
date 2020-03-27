5ae2b43d574d30e8f5dba9d54245da24759c8e49
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-27 13:03:39 +0200

    Lisätty yksinkertaiset C#-esimerkit if- ja for-lauseista.

--- Ohjelmalogiikkaa/Ohjelmalogiikkaa/Program.cs
+++ Ohjelmalogiikkaa/Ohjelmalogiikkaa/Program.cs
@@ -6,7 +6,36 @@ namespace Ohjelmalogiikkaa
     {
         static void Main(string[] args)
         {
+            int i = 100;
+            int j = 200;
+            string s = "ABC";
             Console.WriteLine("Hello World!");
+
+            if (i > 100)
+            {
+                if (i < 500)
+                {
+                    Console.WriteLine("I:n arvo on yli sata mutta alle 500.");
+                }
+                else
+                {
+                    Console.WriteLine("I:n arvo on yli 500.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("I:n arvo on sata tai alle.");
+            }
+
+            for (int laskuri = 0; laskuri < 10; laskuri++)
+            {
+                Console.WriteLine("Moikka!");
+            }
+
+            /*
+            if (i > 100) Console.WriteLine("I:n arvo on yli sata.");
+            else Console.WriteLine("I:n arvo on sata tai alle.");
+            */
         }
     }
 }
