81d0606c068729cb4364538fa5c55f23de5b86d2
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-30 14:49:46 +0300

    Lisätty loki-esimerkki sekä testattu C#-sovellusten muistinhallintaa.

---
+++ OlioOhjelmointi/OlioOhjelmointi/Loki.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OlioOhjelmointi
+{
+    class Loki
+    {
+        public static void Kirjoita(string viesti)
+        {
+            Console.WriteLine(viesti);
+        }
+    }
+}
--- OlioOhjelmointi/OlioOhjelmointi/Program.cs
+++ OlioOhjelmointi/OlioOhjelmointi/Program.cs
@@ -8,6 +8,8 @@ namespace OlioOhjelmointi
         {
             Console.WriteLine("Hello World!");
 
+            Loki.Kirjoita("Sovellus käynnistyy");
+
             Auto oma = new Auto();
             oma.Väri = "Punainen";
             oma.Huippunopeus = 240;
@@ -28,13 +30,31 @@ namespace OlioOhjelmointi
             kuarkki.NostaLava();
 
             Tulosta(kuarkki);
+
+            Loki.Kirjoita("Aloitetaan silmukka...");
+            Console.ReadLine();
+            for (int i = 0; i < 10000000; i++)
+            {
+                TestaaOlioita();
+            }
+
+            Loki.Kirjoita("Sovelluksen suoritus päättyy");
+            Console.ReadLine();
         }
 
         static void Tulosta(Auto a)
         {
+            Loki.Kirjoita("Tulostetaan auto ruudulle");
             Console.WriteLine("Auto: väri = " + a.Väri +
                 ", nopeus = " + a.Nopeus +
                 ", huippunopeus = " + a.Huippunopeus);
+        }
+
+        static void TestaaOlioita()
+        {
+            Auto a = new Auto(); // muistinkulutus n. 50 tavua
+            a.Väri = "Keltainen";
+            a.Huippunopeus = 220;
         }
     }
 }
