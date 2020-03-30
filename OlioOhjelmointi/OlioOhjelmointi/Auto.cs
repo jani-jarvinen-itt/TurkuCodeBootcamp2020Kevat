833b9390409dc31df2322cc78ad7ba03a5deffde
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-30 11:28:07 +0300

    Lisätty auto-luokkaan toimintoja sekä esimerkki nopeuden tilan loogisuuden tarkistamisesta.

--- OlioOhjelmointi/OlioOhjelmointi/Auto.cs
+++ OlioOhjelmointi/OlioOhjelmointi/Auto.cs
@@ -6,22 +6,56 @@ namespace OlioOhjelmointi
 {
     class Auto
     {
-        // ominaisuudet
+        // ominaisuudet (property)
         public int Nopeus { get; set; }
+
+        public int Huippunopeus { get; set; }
 
         public bool MoottoriKäynnissä { get; set; }
 
         public string Väri { get; set; }
 
+        /*
+        // rakentaja eli konstruktori (ei tässä tarpeen, muuttuja alustetaan automaattisesti)
+        public Auto()
+        {
+            Nopeus = 0;
+            MoottoriKäynnissä = false;
+        }
+        */
+
         // metodit eli toiminnot
         public void Käynnistä()
         {
+            PyöritäStarttiMoottoria();
             MoottoriKäynnissä = true;
         }
 
         public void Sammuta()
         {
             MoottoriKäynnissä = false;
+        }
+
+        public void Kiihdytä()
+        {
+            Nopeus += 10;  // Nopeus = Nopeus + 10;
+            if (Nopeus > Huippunopeus)
+            {
+                Nopeus = Huippunopeus;
+            }
+        }
+
+        public void Jarruta()
+        {
+            Nopeus -= 10; // Nopeus = Nopeus - 10;
+            if (Nopeus < 0)
+            {
+                Nopeus = 0;
+            }
+        }
+
+        private void PyöritäStarttiMoottoria()
+        {
         }
     }
 }
--- OlioOhjelmointi/OlioOhjelmointi/Program.cs
+++ OlioOhjelmointi/OlioOhjelmointi/Program.cs
@@ -10,10 +10,13 @@ namespace OlioOhjelmointi
 
             Auto oma = new Auto();
             oma.Väri = "Punainen";
+            oma.Huippunopeus = 240;
             oma.Käynnistä();
+            oma.Kiihdytä();
 
             Auto naapurin = new Auto();
             naapurin.Väri = "Harmaa";
+            naapurin.Huippunopeus = 190;
             naapurin.Käynnistä();
         }
     }
