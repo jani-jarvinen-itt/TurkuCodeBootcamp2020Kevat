66872cc7c911317777011ab6e5fb756335785818
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-30 13:24:02 +0300

    Lisätty esimerkki perityn luokan määrittelystä C#-kielellä.

---
+++ OlioOhjelmointi/OlioOhjelmointi/KuormaAuto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OlioOhjelmointi
+{
+    class KuormaAuto : Auto
+    {
+        public int Kantavuus { get; set; }
+
+        public void NostaLava()
+        {
+
+        }
+    }
+}
--- OlioOhjelmointi/OlioOhjelmointi/Program.cs
+++ OlioOhjelmointi/OlioOhjelmointi/Program.cs
@@ -13,11 +13,28 @@ namespace OlioOhjelmointi
             oma.Huippunopeus = 240;
             oma.Käynnistä();
             oma.Kiihdytä();
+            Tulosta(oma);
 
             Auto naapurin = new Auto();
             naapurin.Väri = "Harmaa";
             naapurin.Huippunopeus = 190;
             naapurin.Käynnistä();
+            Tulosta(naapurin);
+
+            KuormaAuto kuarkki = new KuormaAuto();
+            kuarkki.Väri = "Sininen";
+            kuarkki.Huippunopeus = 110;
+            kuarkki.Kantavuus = 40;
+            kuarkki.NostaLava();
+
+            Tulosta(kuarkki);
+        }
+
+        static void Tulosta(Auto a)
+        {
+            Console.WriteLine("Auto: väri = " + a.Väri +
+                ", nopeus = " + a.Nopeus +
+                ", huippunopeus = " + a.Huippunopeus);
         }
     }
 }
