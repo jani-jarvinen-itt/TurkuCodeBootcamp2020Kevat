fcec741b2d280a0118a7002081f5099ed275e5ca
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-31 09:10:07 +0300

    Lisätty esimerkki kokonaisulukutyyppein laskennasta.

--- OlioOhjelmointi/OlioOhjelmointi/Program.cs
+++ OlioOhjelmointi/OlioOhjelmointi/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace OlioOhjelmointi
 {
@@ -8,6 +10,17 @@ namespace OlioOhjelmointi
         {
             Console.WriteLine("Hello World!");
 
+            byte b1 = 250;
+            byte b2 = 100;
+
+            int summa = b1 + b2;
+            Console.WriteLine(summa); // tulostaa 350
+
+            byte summa2 = (byte)(b1 + b2); // tyyppimuunnos, type cast
+            Console.WriteLine(summa2); // tulostaa 94
+
+
+            /*
             Loki.Kirjoita("Sovellus käynnistyy");
 
             Auto oma = new Auto();
@@ -38,8 +51,17 @@ namespace OlioOhjelmointi
                 TestaaOlioita();
             }
 
+
+            FileStream virta = new FileStream("C:\\Temp\\Tiedosto.txt",
+                FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            byte[] tavut = Encoding.UTF8.GetBytes("Moi maailma!");
+            virta.Write(tavut);
+            // virta.Close();
+
+
             Loki.Kirjoita("Sovelluksen suoritus päättyy");
             Console.ReadLine();
+            */
         }
 
         static void Tulosta(Auto a)
