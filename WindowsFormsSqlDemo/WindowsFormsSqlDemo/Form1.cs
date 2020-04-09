c3cd94401754bf375b0ef0fd3cd2aa08f8891912
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-04-09 09:50:17 +0300

    Lisätty esimerkkisovellus WinForms-tekniikasta sekä ADO.NET-tietokantakäsittelystä SQL Serveriin.

---
+++ WindowsFormsSqlDemo/WindowsFormsSqlDemo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSqlDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Hello World!");
+
+            string yhteysmerkkijono = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+            SqlConnection yhteys = new SqlConnection(yhteysmerkkijono);
+
+            yhteys.Open();
+            MessageBox.Show("Tietokantayhteys avattu");
+
+            yhteys.Close();
+        }
+    }
+}
