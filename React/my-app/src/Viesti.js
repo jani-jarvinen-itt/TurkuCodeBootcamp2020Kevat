e2d62af5fdde3758fe4b3f1a2ccdcd91d7eed440
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-04-15 10:50:31 +0300

    Lisätty uusi React-komponentti: Viesti.

---
+++ React/my-app/src/Viesti.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Viesti extends React.Component {
+    render() {
+        return <h2>Tämä on Viesti-komponentti!</h2>;
+    }
+}
+
+export default Viesti;
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Viesti from './Viesti';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <App />
-    <App />
-    <App />
+    <Viesti />
   </React.StrictMode>,
   document.getElementById('root')
 );
