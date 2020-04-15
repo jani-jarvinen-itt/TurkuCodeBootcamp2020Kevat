8a9b0dde73bcc9047a00726e07b7c2e3d429dab2
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-04-15 10:43:21 +0300

    Poistettu turhat merkinnät HTML-tiedostosta sekä App-komponentista.

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -5,21 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Moi React!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Oma sovellus</h1>
     </div>
   );
 }
